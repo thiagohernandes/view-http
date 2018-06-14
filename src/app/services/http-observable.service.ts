9735c01d7ac456c4053fe23bdddbec5f9840a0a0
Thiago Hernandes <thiago.hernandes@shift.com.br>
2018-06-14 14:12:37 -0300

    Ajustes de tab e observable

--- src/app/services/http-observable.service.ts
+++ src/app/services/http-observable.service.ts
@@ -1,27 +1,32 @@
 
 import {throwError as observableThrowError, Observable} from 'rxjs';
 
-import {catchError} from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 
 
 
 import { Item } from '../model/item';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class HttpObservableService {
 
   private apiRoot = 'http://jsonplaceholder.typicode.com/posts/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   consultaOrdemServico(): Observable<Item[]> {
     return this.http.get<Item[]>(this.apiRoot).pipe(
-                    catchError(this._errorHandler));
+                      tap(() => { console.log('Ok'); this.router.navigate(['/app-simple-http']);
+                      }),
+                      catchError(this._errorHandler));
   }
 
   _errorHandler(error: HttpErrorResponse) {
+    // tslint:disable-next-line:no-debugger
+    debugger;
     return observableThrowError(error.message || 'Server Error');
   }
 
